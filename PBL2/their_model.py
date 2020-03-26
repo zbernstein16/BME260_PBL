1b207c5501c3b1287ffac4a83e223cb685e0a92d
Zach Bernstein <zb32@duke.edu>
2020-03-26 15:37:47 -0400

    Created combined_model.py which begins renaming variables under our scheme and includes basic monocyte broduction and infection

--- PBL2/their_model.py
+++ PBL2/their_model.py
@@ -21,56 +21,57 @@ N_C = 15
 d_5 = 6.6
 
 
-T1_0 = 1e6
-T2_0 = 5e7
-T1x_0 = 0
-T2x_0 = 0
+T_B_0 = 1e6
+T_L_0 = 5e7
+T_Bx_0 = 0
+T_Lx_0 = 0
 Mx_0 = 0
 V1_0 = 1e-3
 V2_0 = 1e-3
 C_0 = 0
 
 
+
 def model(X,t):
-    T1 = X[0]
-    T2 = X[1]
-    T1x = X[2]
-    T2x = X[3]
+    T_B = X[0]
+    T_L = X[1]
+    T_Bx = X[2]
+    T_Lx = X[3]
     Mx = X[4]
     V1 = X[5]
     V2 = X[6]
     C = X[7]
 
-    dT1 = lam_1 - k*V1*T1 -d_1*T1 - sig_1*(1+gam_r*C)*T1 + sig_2*T2
-    dT2 = lam_2 + sig_1*(1+gam_r*C)*T1 - k*V2*T2 - sig_2*T2 - d_1*T2
-    dT1x = k*V1*T1 - d_2*T1x
-    dT2x = (1-f)*k*V2*T2 - d_2*T2x
-    dMx = f*k*V2*T2 - d_3*Mx
-    dV1 = p_v1*T1x - d_4*V1 + D_1*(V2 - V1)
-    dV2 = p_v2*T2x - d_4*V2 + D_2*(V1 - V2)
+    dT_B = lam_1 - k*V1*T_B -d_1*T_B - sig_1*(1+gam_r*C)*T_B + sig_2*T_L
+    dT_L = lam_2 + sig_1*(1+gam_r*C)*T_B - k*V2*T_L - sig_2*T_L - d_1*T_L
+    dT_Bx = k*V1*T_B - d_2*T_Bx
+    dT_Lx = (1-f)*k*V2*T_L - d_2*T_Lx
+    dMx = f*k*V2*T_L - d_3*Mx
+    dV1 = p_v1*T_Bx - d_4*V1 + D_1*(V2 - V1)
+    dV2 = p_v2*T_Lx - d_4*V2 + D_2*(V1 - V2)
     dC = N_C*d_3*Mx - d_5*C
 
-    return [dT1,dT2,dT1x,dT2x,dMx,dV1,dV2,dC]
+    return [dT_B,dT_L,dT_Bx,dT_Lx,dMx,dV1,dV2,dC]
 
 ### Simulation
 x0 = [
-T1_0,
-T2_0,
-T1x_0,
-T2x_0,
+T_B_0,
+T_L_0,
+T_Bx_0,
+T_Lx_0,
 Mx_0,
 V1_0,
 V2_0,
 C_0]
 
-t = np.linspace(0,365,1e6)
+t = np.linspace(0,365*10,1e6)
 sol,info = odeint(model, x0, t,full_output=True)
 
 
-T1 = sol[:,0]
-T2 = sol[:,1]
-T1x = sol[:,2]
-T2x = sol[:,3]
+T_B = sol[:,0]
+T_L = sol[:,1]
+T_Bx = sol[:,2]
+T_Lx = sol[:,3]
 Mx = sol[:,4]
 V1 = sol[:,5]
 V2 = sol[:,6]
@@ -78,8 +79,8 @@ C = sol[:,7]
 
 plt.figure(1)
 
-plt.plot(t,T1,'k-')
-plt.plot(t,T1x,'r-')
+plt.plot(t,T_B,'k-')
+plt.plot(t,T_Bx,'r-')
 
 
 
@@ -88,8 +89,8 @@ plt.show()
 plt.figure(1)
 
 plt.subplot(1,2,1)
-plt.plot(t,T2,'k-')
-plt.plot(t,T2x,'r-')
+plt.plot(t,T_L,'k-')
+plt.plot(t,T_Lx,'r-')
 plt.plot(t,Mx,'b-')
 
 plt.subplot(1,2,2)
